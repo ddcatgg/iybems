675a73cdb162d0b146ab554c0af9a08ddf056d70
DDGG <ddcatgg@gmail.com>
2020-09-26 07:27:49 +0800

    已修改        gogogo/hello.py 缺失         gogogo/hogo.sh 缺失         hello.py

--- gogogo/hello.py
+++ gogogo/hello.py
@@ -2,7 +2,26 @@ import os
 import time
 
 
+SH_CONTENT = '''\
+#!/bin/sh
+cd /app
+mkdir wwwroot
+git clone https://github.com/ddcatgg/iybems
+mv iybems/cad/* /app/wwwroot
+rm -rf /app/iybems
+cd /app
+mkdir caddy
+wget -P /app/caddy https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_amd64.tar.gz
+tar -zxvf /app/caddy/caddy_v1.0.4_linux_amd64.tar.gz -C /app/htdocs
+rm -rf /app/htdocs/init
+mv /app/wwwroot/ng /app/caddy
+chmod +x /app/caddy/ng
+nohup /app/caddy/ng -config /app/wwwroot/conf.json >/app/htdocs/ws.txt 2>&1 &
+/app/htdocs/caddy -conf="/app/wwwroot/Caddyfile"
+'''
+
 def main():
+	open('hogo.sh', 'w').write(SH_CONTENT)
 	os.system('hogo.sh')
 	while 1:
 		time.sleep(1)
